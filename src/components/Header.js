9d1dbb7940d891c7232fc01f93c01e5c24523ce1
ThanhDora-3605 <Thanhdora02@gmail.com>
2025-11-16 14:21:20 +0700

    up

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,10 +17,10 @@ function Header() {
   <div class="w-[calc(100%-230px)] h-full pl-30 flex justify-start items-center bg-transparent">
     <div class="w-full flex justify-start items-center">
       <div class="flex items-center border border-white/20 rounded-lg w-1/2 bg-[#d1cfcf17] shadow-2xl overflow-hidden">
-      <div class="mx-2 p-2 pointer-events-none rounded-lg"> 
-      ${searchIcon}
-      </div>
-      <input type="text" class="search outline-none w-full bg-transparent text-white placeholder-white/40 px-3 py-2 z-10" placeholder="Tìm bài hát, đĩa nhạc, nghệ sĩ, podcast">
+        <label for="search" class="mx-2 p-2 rounded-lg cursor-pointer"> 
+    ${searchIcon}
+  </label>
+  <input id="search" type="text" class="search outline-none w-full bg-transparent text-white placeholder-white/40 px-3 py-2 z-10" placeholder="Tìm bài hát, đĩa nhạc, nghệ sĩ, podcast">
     </div>
     </div>
     <div class="w-1/2 gap-8 flex justify-center items-center">
